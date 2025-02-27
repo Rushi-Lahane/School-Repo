7357b95cb4ec7783966487fe4a94ccd21a37ba00
Rushikesh Lahane <rishipatil3131@gmail.com>
2025-02-27 12:39:01 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -24,7 +24,7 @@ const About = () => {
 
   return (
     <>
-    <CommonTopBanner title={"About"}/>
+    <CommonTopBanner title={'About'}/>
       <div className='bg-gray-100 py-10'>
         <div className='w-11/12 mx-auto grid grid-cols-1 sm:grid-cols-12 gap-y-5 sm:gap-x-4'>
           {/* side bar button */}
