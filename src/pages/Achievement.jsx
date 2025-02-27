aa39cedd55682e67a20eaa74214192ea2d69cd7f
Rushi-Lahane <rishipatil3131@gmail.com>
2025-02-27 11:16:50 +0530

    change Achievement page heading to test git

--- src/pages/Achievement.jsx
+++ src/pages/Achievement.jsx
@@ -6,7 +6,9 @@ const Achievement = () => {
     <>
     <CommonTopBanner title='Achievement'/>
     <div className='h-50 bg-gray-100 flex items-center justify-center'>
-      <h1 className='text-4xl text-blue-900 font-bold'>Page is Under Construction</h1>
+      <h1 className='text-4xl text-blue-900 font-bold'>Page is Under Construction
+        past 12 vi
+      </h1>
     </div>
     </>
   )
