4dedb384134a7928b97fa1bef59e2bcf1f2da4da
Rushikesh Lahane <rishipatil3131@gmail.com>
2025-02-27 12:43:45 +0530

    Update CommonTopBanner.jsx

--- src/Component/CommonTopBanner.jsx
+++ src/Component/CommonTopBanner.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CommonTopBanner = ({title}) => {
     return (
-        <div className='bg-[url(./public/images/subheaderbg.jpg)] bg-no-repeat bg-cover'>
+        <div className='bg-[url(./images/subheaderbg.jpg)] bg-no-repeat bg-cover'>
             <h3 className='text-center text-blue-900 font-bold  text-4xl py-10'>{title}</h3>
         </div>
     ) 
